f1008a27b15360842f491c29de7b64a80049912a
bhumi dolariya <dolariyabhumi29@gmail.com>
2026-07-09 18:51:55 +0530

    update repo name

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/_Glowcart">
+    <BrowserRouter basename="/glow-cart">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
  plugins: [react(), tailwindcss()],
- base: "/_Glowcart/",  
+ base: "/glow-cart/",  
 });
